d2c9a58194ef4a9c5b716a2023e9b6f6d26b3013
giovaniluzz <giovanirafaah@gmail.com>
2026-09-10 01:12:51 +0000

    Codigos dia 09/09

---
+++ aula005/Principal.java
@@ -0,0 +1,7 @@
+public class Principal{
+    public static void main(String[] args) {
+        ListaDupla<String> lista = new ListaDupla<String>("Alunos");
+
+        lista.addInicio("Rafael");
+    }
+}
